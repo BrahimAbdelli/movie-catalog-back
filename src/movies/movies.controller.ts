abe6d134c7c2921cd1f58624f9c443bb5a9ada90
brahiiim <56367267+brahiiim@users.noreply.github.com>
2021-01-23 10:02:48 +0100

    Create Movie
    Adding the post movie method in controller and service, it will use the Entity, Repository, utils and DTO.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { createMovieDto } from './dto/create-movie.dto';
+import { MovieEntity } from './movie.entity';
+import { MoviesService } from './movies.service';
+import { uploadFile } from './shared/file-upload.utils';
+import { validateImages } from './shared/filters.utils';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(
+    @InjectRepository(MovieEntity)
+    private readonly movieRepository: Repository<MovieEntity>,
+    private readonly movieService: MoviesService,
+  ) {}
+  @Post()
+  @UseInterceptors(FileInterceptor('image'))
+  async create(@Body() movieData: createMovieDto, @UploadedFile() image) {
+    if (image) {
+      validateImages(image);
+      movieData.image = await uploadFile(image);
+    }
+
+    return await this.movieService.create(movieData);
+  }
+}
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { createMovieDto } from './dto/create-movie.dto';
+import { MovieEntity } from './movie.entity';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(
+    @InjectRepository(MovieEntity)
+    private readonly movieRepository: Repository<MovieEntity>,
+  ) {}
+
+  async create(dto: createMovieDto): Promise<MovieEntity> {
+    const newMovie = Object.assign(new MovieEntity(), dto);
+    return await this.movieRepository.save(newMovie);
+  }
+}
