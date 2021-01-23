c471f41d084cd3bb994ae440328d1c913475dc24
brahiiim <56367267+brahiiim@users.noreply.github.com>
2021-01-23 09:32:43 +0100

    Updating app module
    Correcting the controllers import arguments, adding Orm, Cache, and databse specifications to the imports decorator.
    
    Installing the cache-manager:
    npm install cache-manager
    
    The app now requires database connection to run.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.register(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logger: 'advanced-console',
+      useUnifiedTopology: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
