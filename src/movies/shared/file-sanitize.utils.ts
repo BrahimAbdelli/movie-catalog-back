293427c3dd1bfec2a0f7b7afbcf21e65eb2ba4b5
brahiiim <56367267+brahiiim@users.noreply.github.com>
2021-01-23 09:18:49 +0100

    Adding the sanitizer util
    Adding the sanitize util to remove directory paths and invalid characters

---
+++ src/movies/shared/file-sanitize.utils.ts
@@ -0,0 +1,13 @@
+import { extname } from 'path';
+
+export function sanitizeFileName(originalname) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const sanitize = require('sanitize-filename');
+  const name = sanitize(originalname.split('.')[0]);
+  const fileExtName = extname(originalname);
+  const randomName = Array(4)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+  return `${name}-${randomName}${fileExtName}`;
+}
